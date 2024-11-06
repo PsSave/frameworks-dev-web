b0ce9c68fcb3297f21a877ee5e44908bd7f7d79f
Pedro Samuel <pedrosviki1265@gmail.com>
2024-11-06 18:27:30 -0400

    Adiciona página inicial com menu e novas rotas para canva, webcam e lista

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('menu.html')
 
 @app.route('/canva')
 def canvas():
@@ -15,6 +15,9 @@ def canvas():
 def webcam():
     return render_template('webcam.html')
 
+@app.route('/list')
+def list():
+    return render_template('list.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
